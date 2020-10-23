70c25eb085cb0248fcf03e8dba5c629a8786279c
Roman <romanb@slatestudio.com>
2020-10-23 22:57:41 +0800

    fix: pass missing `keyStorageUrl` into `signUp`. Bump SDK 2.0.1 (#19)

--- sdk/core/test/helpers/generateUsername.ts
+++ sdk/core/test/helpers/generateUsername.ts
@@ -3,3 +3,9 @@ export const generateUsername = () => {
 
   return `test.user-${TIMESTAMP}`
 }
+
+export const generateEmail = () => {
+  const TIMESTAMP = Date.now().toString(16).toUpperCase()
+
+  return `test.user-${TIMESTAMP}@mail.com`
+}
