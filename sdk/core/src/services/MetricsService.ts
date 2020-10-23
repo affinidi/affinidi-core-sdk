d40c97c7056cacb722d7bd064c577dc6945c5017
Kyle Chung <kylechun9@gmail.com>
2020-10-23 15:52:59 +0800

    move vc metadata parser from metrics-lib to core

--- sdk/core/src/services/MetricsService.ts
+++ sdk/core/src/services/MetricsService.ts
@@ -1,17 +1,33 @@
 import { profile } from '@affinidi/common'
-import {
-  metrics,
-  EventComponent,
-  VcMetadata,
-  SpecificVcMetadada,
-  VcMetadataParser,
-  DefaultVcMetadataParser,
-} from '@affinidi/affinity-metrics-lib'
+import { metrics, EventComponent, VcMetadata } from '@affinidi/affinity-metrics-lib'
 
 import { MetricsEvent, MetricsServiceOptions, SignedCredential } from '../dto/shared.dto'
 
+export type CommonVcMetadata = Omit<VcMetadata, 'data'>  // anything to data will be overwritten by SpecificVcMetadada.data
+export type SpecificVcMetadada = { data: any }
+
+class VcMetadataParser {
+
+  // parse vcType-agnostic metadata
+  private parseCommon(credential: any): CommonVcMetadata {
+    const metadata = { vcType: credential.type }
+    return metadata
+  }
+
+  // parse vcType-specific metadata
+  parseSpecific(credential: any): SpecificVcMetadada {
+    return { data: {} }
+  }
+
+  parse(credential: any): any {
+    const baseMetadata = this.parseCommon(credential)
+    const addOnMetadata = this.parseSpecific(credential)
+    return {...baseMetadata, ...addOnMetadata}
+  }
+}
+
 // TODO: move to a single file for all vcType-specific parsers?
-class HealthPassportVCParser extends VcMetadataParser {
+class HealthPassportParser extends VcMetadataParser {
   parseSpecific(credential: any): SpecificVcMetadada {
     const targetResources = ['Specimen', 'Observation', 'Organization']
     const entriesIn = credential.credentialSubject.data.fhirBundle.entry
@@ -26,9 +42,9 @@ class VcMetadataParserFactory {
   createParser(vcType: string): VcMetadataParser {
     switch (vcType) {
       case 'HealthPassportBundleCredentialV1': // TODO: can we import this value from vc-data in a modular way?
-        return new HealthPassportVCParser()
+        return new HealthPassportParser()
       default:
-        return new DefaultVcMetadataParser()
+        return new VcMetadataParser()
     }
   }
 }
