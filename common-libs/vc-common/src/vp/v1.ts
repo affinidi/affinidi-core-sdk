f791a1601eacdc5495ab19afe9356797fc18e25e
Eddie Hedges <eddiehedges@gmail.com>
2020-10-26 16:00:25 -0700

    Add types for presentation_submission & used in VPV1Unsigned

--- common-libs/vc-common/src/vp/v1.ts
+++ common-libs/vc-common/src/vp/v1.ts
@@ -17,17 +17,29 @@ export type VPV1Holder = {
   id: string
 }
 
+export type PresentationSubmissionDescriptorV1 = {
+  id: string
+  path: string
+  path_nested?: PresentationSubmissionDescriptorV1
+  format: 'jwt' | 'jwt_vc' | 'jwt_vp' | 'ldp' | 'ldp_vc' | 'ldp_vp'
+}
+export type PresentationSubmissionV1 = {
+  locale?: string
+  descriptor_map: PresentationSubmissionDescriptorV1[]
+}
+
 export type VPV1Unsigned<
   VC extends VCV1 = VCV1,
   Type extends VPV1Type = VPV1Type,
   Holder extends VPV1Holder = VPV1Holder
-> = {
-  '@context': TContext
-  id?: string
-  type: Type
-  verifiableCredential: VC[]
-  holder: Holder
-}
+  > = {
+    '@context': TContext
+    id?: string
+    type: Type
+    verifiableCredential: VC[]
+    holder: Holder
+    presentation_submission?: PresentationSubmissionV1
+  }
 
 // TODO: This is missing the `signature` and `packedData` fields. How should those translate over?
 export type VPV1<
@@ -35,6 +47,6 @@ export type VPV1<
   Type extends VPV1Type = VPV1Type,
   Holder extends VPV1Holder = VPV1Holder,
   Proof extends VPV1Proof = VPV1Proof
-> = VPV1Unsigned<VC, Type, Holder> & {
-  proof: Proof
-}
+  > = VPV1Unsigned<VC, Type, Holder> & {
+    proof: Proof
+  }
