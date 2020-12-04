7a9560c9e70a2a3fd89afac5fbcc54003b1247d3
Kyle Chung <kyle.c@affinidi.com>
2020-12-04 16:46:41 +0800

    [NEP-1232] add event for vp, refactor metrics service methods for sending event (#55)
    * add event for vp, refactor metrics service methods for sending event
    
    * add instance method for signing vp in jwt
    
    * fix compile error
    
    * bump ver
    
    * lint
    
    * update unit test for metrics service
    
    * add default subcategory value since it is required at the metrics backend
    
    * do not pass promise to metrics send
    
    * add unit test for vp jwt signing with instance method
    
    * change back jwt obj typ mock value
    
    * remove local test artifacts
    
    * vp id as the 2nd link for vp signing event
    
    * skip int test for revoke for now
    
    * lint

--- common-libs/common/test/unit/services/MetricsService.test.ts
+++ common-libs/common/test/unit/services/MetricsService.test.ts
@@ -5,6 +5,8 @@ import MetricsService from '../../../src/services/MetricsService'
 import { medicalCredential } from '../../factory/medicalCredential'
 import { signedCredential as otherCredential } from '../../factory/signedCredential'
 
+import { EventName } from '@affinidi/affinity-metrics-lib'
+
 const accessApiKey = 'dummyHash'
 const metricsUrl = 'https://dummy'
 
@@ -14,8 +16,9 @@ describe('MetricsService', () => {
   describe('parseVcMetadata', () => {
     const vcType = 'HealthPassportBundleCredentialV1'
 
-    it(`#parse ${vcType}`, async () => {
-      const metadata = metricsService.parseVcMetadata(medicalCredential)
+    const eventVcSaved = EventName.VC_SAVED
+    it(`#parse ${vcType} for ${eventVcSaved} the type-specific metadata`, async () => {
+      const metadata = metricsService.parseVcMetadata(medicalCredential, eventVcSaved)
       const resourceNames = []
 
       for (const entry of metadata.data) {
@@ -27,10 +30,18 @@ describe('MetricsService', () => {
       expect(metadata.vcType[1]).to.equal(vcType)
     })
 
+    const eventVcSigned = EventName.VC_SIGNED
+    it(`#parse ${vcType} for ${eventVcSigned} only the common metadata`, async () => {
+      const metadata = metricsService.parseVcMetadata(medicalCredential, eventVcSigned)
+
+      expect(metadata.vcType[1]).to.equal(vcType)
+      expect(metadata.data).to.be.empty
+    })
+
     const otherType = 'NameCredentialPersonV1'
 
     it(`#parse ${otherType}`, async () => {
-      const metadata = metricsService.parseVcMetadata(otherCredential)
+      const metadata = metricsService.parseVcMetadata(otherCredential, EventName.VC_SAVED)
 
       expect(metadata.data).to.be.empty
       expect(metadata.vcType[1]).to.equal(otherType)
