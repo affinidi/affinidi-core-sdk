6b7ff3dde0309c81da33131c3c434d6bc8e06c5e
Vitaly R <vitaly.r@affinidi.com>
2021-05-20 11:12:32 +0300

    Refactored TestmailHelper

--- sdk/core/test/helpers/TestmailHelper.ts
+++ sdk/core/test/helpers/TestmailHelper.ts
@@ -2,6 +2,7 @@ import fetch from 'node-fetch'
 import { URL } from 'url'
 
 const TESTMAIL_API_URL = 'https://api.testmail.app/api/json'
+const TESTMAIL_INBOX_DOMAIN = 'inbox.testmail.app'
 const TESTMAIL_API_KEY = process.env.TESTMAIL_API_KEY
 const TESTMAIL_NAMESPACE = process.env.TESTMAIL_NS
 
@@ -15,13 +16,13 @@ interface TestmailEmail {
 //       since there are limits for emails sent per month
 //       Use generateEmail() for random email generation
 export class TestmailHelper {
-  public static generateEmailForTag = (tag: string): string => `${TESTMAIL_NAMESPACE}.${tag}@inbox.testmail.app`
+  public static generateEmailForTag = (tag: string): string => `${TESTMAIL_NAMESPACE}.${tag}@${TESTMAIL_INBOX_DOMAIN}`
 
-  public static waitForNewEmail = async (queryTag: string, timestampFrom: number): Promise<TestmailEmail> => {
+  public static waitForNewEmail = async (tag: string, timestampFrom: number): Promise<TestmailEmail> => {
     const url = new URL(TESTMAIL_API_URL)
     url.searchParams.append('apikey', TESTMAIL_API_KEY)
     url.searchParams.append('namespace', TESTMAIL_NAMESPACE)
-    url.searchParams.append('tag', queryTag)
+    url.searchParams.append('tag', tag)
     url.searchParams.append('timestamp_from', String(timestampFrom))
     url.searchParams.append('livequery', 'true')
 
