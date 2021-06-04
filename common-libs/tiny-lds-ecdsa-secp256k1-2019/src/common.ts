da012d8679062392980f0345221dc276e264a5bf
inga-affinidi <82827564+inga-affinidi@users.noreply.github.com>
2021-06-04 14:31:39 +0200

    Restored WithImplicitCoercion type declaration for non-node environments

--- common-libs/tiny-lds-ecdsa-secp256k1-2019/src/common.ts
+++ common-libs/tiny-lds-ecdsa-secp256k1-2019/src/common.ts
@@ -1,6 +1,9 @@
 export const KEY_TYPE = 'EcdsaSecp256k1Signature2019'
 export const VERIFICATION_KEY_TYPE = 'EcdsaSecp256k1VerificationKey2019'
 
+// It is only declared in standard TS type declarations for Node, and we need this to run everywhere
+type WithImplicitCoercion<T> = T | { valueOf(): T }
+
 export type BufferLike = {
   buffer: WithImplicitCoercion<ArrayBuffer | SharedArrayBuffer>
   byteOffset?: number
