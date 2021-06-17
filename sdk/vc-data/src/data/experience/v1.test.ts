822567ae12f35c728d62a0100da5925fb4dd0264
Sridharan Jayabal <sridharan@docon.co.in>
2021-06-17 20:15:22 +0530

    test snapshots for vc

--- sdk/vc-data/src/data/experience/v1.test.ts
+++ sdk/vc-data/src/data/experience/v1.test.ts
@@ -30,6 +30,111 @@ describe('VCExperiencePersonV1', () => {
       context: getVCExperiencePersonV1Context(),
     })
 
-    expect(expanded).toMatchInlineSnapshot()
+    expect(expanded).toMatchInlineSnapshot(`
+      Object {
+        "@id": "urn:uuid:9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+        "@type": Array [
+          "https://www.w3.org/2018/credentials#VerifiableCredential",
+          "https://schema.affinity-project.org/ExperienceCredentialPersonV1",
+        ],
+        "https://www.w3.org/2018/credentials#credentialSubject": Array [
+          Object {
+            "https://schema.affinity-project.org/data": Array [
+              Object {
+                "@type": Array [
+                  "https://schema.org/Person",
+                  "https://schema.affinity-project.org/PersonE",
+                  "https://schema.affinity-project.org/ExperiencePerson",
+                ],
+                "https://schema.org/EmployeeAddress": Array [
+                  Object {
+                    "@value": "9, 9th street, Koramangala, Bangalore",
+                  },
+                ],
+                "https://schema.org/dateOfJoining": Array [
+                  Object {
+                    "@value": "15-02-2021",
+                  },
+                ],
+                "https://schema.org/dateOfRelieving": Array [
+                  Object {
+                    "@value": "relieving date here",
+                  },
+                ],
+                "https://schema.org/employmentType": Array [
+                  Object {
+                    "@value": "Full-time",
+                  },
+                ],
+                "https://schema.org/name": Array [
+                  Object {
+                    "@value": "Bob Belcher",
+                  },
+                ],
+                "https://schema.org/officialEmail": Array [
+                  Object {
+                    "@value": "bob.b@affinidi.com",
+                  },
+                ],
+                "https://schema.org/organizationAddress": Array [
+                  Object {
+                    "@value": "9, 9th street, Koramangala, Bangalore",
+                  },
+                ],
+                "https://schema.org/organizationLogo": Array [
+                  Object {
+                    "@value": "logo url here",
+                  },
+                ],
+                "https://schema.org/organizationName": Array [
+                  Object {
+                    "@value": "Affinidi",
+                  },
+                ],
+                "https://schema.org/otherDetails": Array [
+                  Object {
+                    "https://schema.org/projects": Array [
+                      Object {
+                        "@value": "onboarding",
+                      },
+                    ],
+                  },
+                ],
+                "https://schema.org/personalEmail": Array [
+                  Object {
+                    "@value": "bob@gmail.com",
+                  },
+                ],
+                "https://schema.org/phoneNumber": Array [
+                  Object {
+                    "@value": "123456789",
+                  },
+                ],
+                "https://schema.org/responsibilities": Array [
+                  Object {
+                    "@value": "writing code",
+                  },
+                ],
+                "https://schema.org/team": Array [
+                  Object {
+                    "@value": "gde",
+                  },
+                ],
+                "https://schema.org/title": Array [
+                  Object {
+                    "@value": "Software Engineer",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        "https://www.w3.org/2018/credentials#holder": Array [
+          Object {
+            "@id": "did:elem:123",
+          },
+        ],
+      }
+    `)
   })
 })
