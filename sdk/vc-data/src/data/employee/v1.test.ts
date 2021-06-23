fbde900ed895e2c31b0b49022deaddd7e668be6f
Sridharan Jayabal <sridharan.219@gmail.com>
2021-06-23 13:11:29 +0530

    Gde custom vc (#103)
    * gde-611, gde-612, gde-613: employee vc, kudos vc, experience vc
    
    * test snapshots for vc
    
    * lint fix
    
    * vc schema changes
    
    * version bump
    
    * capital case fixed
    
    * schema field corrections
    
    * added titleAtExit for experience vc
    
    Co-authored-by: Sridharan Jayabal <sridharan@docon.co.in>

---
+++ sdk/vc-data/src/data/employee/v1.test.ts
@@ -0,0 +1,128 @@
+import { VCEmployeePersonV1, VCSEmployeePersonV1, getVCEmployeePersonV1Context } from './v1'
+import { expandVC } from '../../testUtil.test'
+
+describe('VCEmployeePersonV1', () => {
+  it('expands correctly', async () => {
+    expect.assertions(1)
+
+    const expanded = await expandVC<VCEmployeePersonV1, VCSEmployeePersonV1>({
+      type: 'EmployeeCredentialPersonV1',
+      data: {
+        '@type': ['Person', 'PersonE', 'EmployeePerson'],
+        name: 'Bob Belcher',
+        officialEmail: 'bob.b@affinidi.com',
+        organizationName: 'Affinidi',
+        title: 'Software Engineer',
+        employmentType: 'Full-time',
+        employeeID: '12345',
+        dateOfJoining: '15-02-2021',
+        employeeAddress: '9, 9th street, Koramangala, Bangalore',
+        organizationAddress: '9, 9th street, Koramangala, Bangalore',
+        organizationLogo: 'logo url here',
+        team: 'gde',
+        responsibilities: 'writing code',
+        otherDetails: {
+          projects: ['onboarding'],
+        },
+      },
+      context: getVCEmployeePersonV1Context(),
+    })
+
+    expect(expanded).toMatchInlineSnapshot(`
+      Object {
+        "@id": "urn:uuid:9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+        "@type": Array [
+          "https://www.w3.org/2018/credentials#VerifiableCredential",
+          "https://schema.affinity-project.org/EmployeeCredentialPersonV1",
+        ],
+        "https://www.w3.org/2018/credentials#credentialSubject": Array [
+          Object {
+            "https://schema.affinity-project.org/data": Array [
+              Object {
+                "@type": Array [
+                  "https://schema.org/Person",
+                  "https://schema.affinity-project.org/PersonE",
+                  "https://schema.affinity-project.org/EmployeePerson",
+                ],
+                "https://schema.org/dateOfJoining": Array [
+                  Object {
+                    "@value": "15-02-2021",
+                  },
+                ],
+                "https://schema.org/employeeAddress": Array [
+                  Object {
+                    "@value": "9, 9th street, Koramangala, Bangalore",
+                  },
+                ],
+                "https://schema.org/employeeID": Array [
+                  Object {
+                    "@value": "12345",
+                  },
+                ],
+                "https://schema.org/employmentType": Array [
+                  Object {
+                    "@value": "Full-time",
+                  },
+                ],
+                "https://schema.org/name": Array [
+                  Object {
+                    "@value": "Bob Belcher",
+                  },
+                ],
+                "https://schema.org/officialEmail": Array [
+                  Object {
+                    "@value": "bob.b@affinidi.com",
+                  },
+                ],
+                "https://schema.org/organizationAddress": Array [
+                  Object {
+                    "@value": "9, 9th street, Koramangala, Bangalore",
+                  },
+                ],
+                "https://schema.org/organizationLogo": Array [
+                  Object {
+                    "@value": "logo url here",
+                  },
+                ],
+                "https://schema.org/organizationName": Array [
+                  Object {
+                    "@value": "Affinidi",
+                  },
+                ],
+                "https://schema.org/otherDetails": Array [
+                  Object {
+                    "https://schema.org/projects": Array [
+                      Object {
+                        "@value": "onboarding",
+                      },
+                    ],
+                  },
+                ],
+                "https://schema.org/responsibilities": Array [
+                  Object {
+                    "@value": "writing code",
+                  },
+                ],
+                "https://schema.org/team": Array [
+                  Object {
+                    "@value": "gde",
+                  },
+                ],
+                "https://schema.org/title": Array [
+                  Object {
+                    "@value": "Software Engineer",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+        "https://www.w3.org/2018/credentials#holder": Array [
+          Object {
+            "@id": "did:elem:123",
+          },
+        ],
+      }
+    `)
+  })
+})
---
+++ sdk/vc-data/src/data/experience/v1.ts
@@ -0,0 +1,57 @@
+import { VCV1, VCV1Subject } from '@affinidi/vc-common'
+
+import { PersonEV1, getBaseV1ContextEntries } from '../base/v1'
+import {
+  CreateThing,
+  MaybeArray,
+  ExtendThing,
+  Type,
+  ExpandThing,
+  createContextEntry,
+  createVCContextEntry,
+} from '../util'
+
+type ExperiencePersonV1Mixin = CreateThing<
+  'ExperiencePerson',
+  {
+    name: string
+    organizationName: string
+    titlesHeld: MaybeArray<string>
+    titleAtExit: string
+    employmentType: 'Full-time' | 'Part-time' | 'Contractor' | 'Intern'
+    dateOfJoining: string
+    personalEmail?: string
+    phoneNumber?: string
+    employeeAddress?: string
+    organizationAddress?: string
+    organizationLogo?: string
+    dateOfRelieving?: string
+    team?: string
+    responsibilities?: string
+    otherDetails?: any
+  }
+>
+
+export type ExperiencePersonV1 = ExtendThing<ExperiencePersonV1Mixin, PersonEV1>
+
+export type VCSExperiencePersonV1 = VCV1Subject<ExpandThing<ExperiencePersonV1>>
+
+export type VCExperiencePersonV1 = VCV1<VCSExperiencePersonV1, Type<'ExperienceCredentialPersonV1'>>
+
+export const getVCExperiencePersonV1Context = () => {
+  const ExperiencePersonEntry = createContextEntry<ExperiencePersonV1Mixin, PersonEV1>({
+    type: 'ExperiencePerson',
+    typeIdBase: 'affSchema',
+    fields: {
+      medal: 'schema',
+    },
+    vocab: 'schema',
+  })
+
+  return createVCContextEntry<VCExperiencePersonV1>({
+    type: 'ExperienceCredentialPersonV1',
+    typeIdBase: 'affSchema',
+    entries: [ExperiencePersonEntry, ...getBaseV1ContextEntries()],
+    vocab: 'schema',
+  })
+}
