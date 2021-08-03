205bbf155337cb042e1687e3fe73b7fb73846b60
inga-affinidi <82827564+inga-affinidi@users.noreply.github.com>
2021-08-03 18:47:57 +0200

    Exported more required types

--- sdk/core/src/index.ts
+++ sdk/core/src/index.ts
@@ -1,7 +1,7 @@
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
 export { EventComponent } from '@affinidi/affinity-metrics-lib'
-export type { VCV1Unsigned, VPV1Unsigned } from '@affinidi/vc-common'
+export type { VCV1, VCV1Unsigned, VPV1Unsigned } from '@affinidi/vc-common'
 export type { CredentialRequirement, DidMethod, Env, MessageParameters, SdkOptions } from './dto/shared.dto'
 export { createV5CompatibleWalletFactories, createV6WalletFactories } from './factories/walletFactories'
 export { Util } from './CommonNetworkMember/Util'
--- sdk/node/src/index.ts
+++ sdk/node/src/index.ts
@@ -1,6 +1,6 @@
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
 import type * as Types from '@affinidi/wallet-core-sdk'
-export { Util } from '@affinidi/wallet-core-sdk'
+export { EventComponent, Util } from '@affinidi/wallet-core-sdk'
 export { createWallet } from './AffinityWallet'
 export { Types }
