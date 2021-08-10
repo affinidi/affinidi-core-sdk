1324dd57e5bb9e1892e736f1f234aba26e1228af
HItesh Joshi <hitesh.j@AFFINITYMAC-075.local>
2021-08-10 15:05:35 +0800

    feat: Add Hackathon VC with awardDate

---
+++ sdk/vc-data/src/data/hackathon/v1.ts
@@ -0,0 +1,57 @@
+import { VCV1, VCV1Subject } from '@affinidi/vc-common'
+import {
+    createContextEntry,
+    CreateThing,
+    createVCContextEntry,
+    ExpandThing,
+    ExtendThing,
+    Type
+} from '../util'
+
+
+type HackathonEV1Mixin = CreateThing<'HackathonE'>
+
+export type HackathonEV1 = ExtendThing<HackathonEV1Mixin, CreateThing<'Hackathon'>>
+
+export const getBaseHackathonContextEntries = () => {
+    const hackathonEV1ContextEntry = createContextEntry<HackathonEV1Mixin>({
+        type: 'HackathonE',
+        typeIdBase: 'affSchema',
+        fields: {},
+        vocab: 'schema',
+    })
+
+    return [hackathonEV1ContextEntry];
+}
+
+
+
+
+type HackathonWinnerV1Mixin = CreateThing<'HackathonWinner', {
+    awardDate: Date
+}>
+
+export type HackathonWinnerV1 = ExtendThing<HackathonWinnerV1Mixin, HackathonEV1>
+
+export type VCSHackathonWinnerV1 = VCV1Subject<ExpandThing<HackathonWinnerV1>>
+
+export type VCHackathonWinnerV1 = VCV1<VCSHackathonWinnerV1, Type<'HackathonWinnerCredentialV1'>>
+
+
+export const getVCHackathonWinnerV1Context = () => {
+    const hackathonEntry = createContextEntry<HackathonWinnerV1Mixin, HackathonWinnerV1>({
+        type: 'HackathonWinner',
+        typeIdBase: 'affSchema',
+        fields: {
+            awardDate: 'schema',
+        },
+        vocab: 'schema',
+    })
+
+    return createVCContextEntry<VCHackathonWinnerV1>({
+        type: 'HackathonWinnerCredentialV1',
+        typeIdBase: 'affSchema',
+        entries: [hackathonEntry, ...getBaseHackathonContextEntries()],
+        vocab: 'schema',
+    })
+}
