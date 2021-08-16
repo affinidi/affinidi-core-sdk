f09638834599b870438edb5c14fbd9eec80e6d7a
Jagvir Bains <jagvir.s@affinidi.com>
2021-08-16 17:06:55 +0530

    Fix: Changes to remove code lint errors for Grant VC

--- sdk/vc-data/src/data/grant/v1.ts
+++ sdk/vc-data/src/data/grant/v1.ts
@@ -1,22 +1,15 @@
 import { VCV1, VCV1Subject } from '@affinidi/vc-common'
 
-import {
-    createContextEntry,    
-    CreateThing,
-    createVCContextEntry,
-    ExpandThing,
-    ExtendThing,
-    Type
-} from '../util'
+import { createContextEntry, CreateThing, createVCContextEntry, ExpandThing, ExtendThing, Type } from '../util'
 
 import { getBaseV1ContextEntries } from '../base/v1'
 
 type GrantWinnerV1Mixin = CreateThing<
-    'GrantWinner',
-    {
-        grantBeneficiary: CreateThing<'Organization'>,
-        dateAwarded: String
-    }
+  'GrantWinner',
+  {
+    grantBeneficiary: CreateThing<'Organization'>
+    dateAwarded: string
+  }
 >
 
 export type GrantWinnerV1 = ExtendThing<GrantWinnerV1Mixin, CreateThing<'MonetaryGrant'>>
@@ -25,22 +18,21 @@ export type VCSGrantWinnerV1 = VCV1Subject<ExpandThing<GrantWinnerV1>>
 
 export type VCGrantWinnerV1 = VCV1<VCSGrantWinnerV1, Type<'GrantWinnerCredentialV1'>>
 
-
 export const getVCGrantWinnerCredentialV1Context = () => {
-    const GrantWinnerCredentialEntry = createContextEntry<GrantWinnerV1Mixin, GrantWinnerV1Mixin>({
-        type: 'GrantWinner',
-        typeIdBase: 'affSchema',
-        fields: {
-            grantBeneficiary: 'affSchema',
-            dateAwarded: 'affSchema',            
-        },
-        vocab: 'schema',
-    })
+  const GrantWinnerCredentialEntry = createContextEntry<GrantWinnerV1Mixin, GrantWinnerV1Mixin>({
+    type: 'GrantWinner',
+    typeIdBase: 'affSchema',
+    fields: {
+      grantBeneficiary: 'affSchema',
+      dateAwarded: 'affSchema',
+    },
+    vocab: 'schema',
+  })
 
-    return createVCContextEntry<VCGrantWinnerV1>({
-        type: 'GrantWinnerCredentialV1',
-        typeIdBase: 'affSchema',
-        entries: [GrantWinnerCredentialEntry, ...getBaseV1ContextEntries()],
-        vocab: 'schema',
-    })
+  return createVCContextEntry<VCGrantWinnerV1>({
+    type: 'GrantWinnerCredentialV1',
+    typeIdBase: 'affSchema',
+    entries: [GrantWinnerCredentialEntry, ...getBaseV1ContextEntries()],
+    vocab: 'schema',
+  })
 }
