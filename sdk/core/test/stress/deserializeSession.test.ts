538fcd392cd8d5ee68a1e6ce38c538c45f21fc42
anton-iskryzhytskyi <a.iskryzhytskyi@gmail.com>
2021-11-10 22:27:12 +0100

    test: add stress test script for deserializeSession
    we need that to understand cloud-wallet bottleneck

---
+++ sdk/core/test/stress/deserializeSession.test.ts
@@ -0,0 +1,49 @@
+import pLimit from 'p-limit'
+import { AffinidiWalletV6 } from '../helpers/AffinidiWallet'
+import { getBasicOptionsForEnvironment, testSecrets } from '../helpers'
+
+const OPERATIONS_COUNT = Number(process.env.DESERIALIZE_SESSION_OPERATIONS_COUNT ?? 1000)
+const CONCURRENCY = Number(process.env.DESERIALIZE_SESSION_CONCURRENCY ?? 10)
+
+const { COGNITO_PASSWORD, COGNITO_USERNAME } = testSecrets
+
+const cognitoUsername = COGNITO_USERNAME
+const cognitoPassword = COGNITO_PASSWORD
+
+const options = getBasicOptionsForEnvironment()
+
+const bucketsPoints = [0.5, 0.95, 1.8, 3.4, 5.3, 7.8, 9.3, 11.3, 14.5, 18.3, 27.1, 39.3, 47.8, 64.4, 97.1, 137.3, 250.5]
+
+const buckets = bucketsPoints.reduce((acc, e) => ({ ...acc, [e]: 0 }), { errors: 0 }) as any
+
+describe('#NetworkMember.deserializeSession', () => {
+  it('should successfully deserializeSession in specificTime', async () => {
+    const optionsWithElemDid = Object.assign({}, options, { didMethod: 'elem' } as const)
+    const commonNetworkMember = await AffinidiWalletV6.logInWithPassword(
+      optionsWithElemDid,
+      cognitoUsername,
+      cognitoPassword,
+    )
+    const session = commonNetworkMember.serializeSession()
+    const limit = pLimit(CONCURRENCY)
+
+    const test = (i: number) => async () => {
+      const start = Date.now()
+      await AffinidiWalletV6.deserializeSession(optionsWithElemDid, session).catch((err) => {
+        buckets.errors += 1
+        console.error(err)
+      })
+      const time = Date.now() - start
+      bucketsPoints.forEach((e) => {
+        if (e > time / 1000) {
+          buckets[`${e}`] += 1
+        }
+      })
+
+      console.log(`deserialize:${i}: ${time / 1000}s`)
+    }
+
+    await Promise.all([...new Array(OPERATIONS_COUNT)].map((_, i) => limit(test(i))))
+    console.log(buckets)
+  })
+})
---
+++ sdk/core/test/stress/index.test.ts
@@ -0,0 +1,4 @@
+import 'mocha'
+import '../integration/env'
+
+require('./deserializeSession.test')
