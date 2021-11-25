1e0cf900485016466664b96961d125c68d023b68
Oleksii Piliugin <oleksii.p@affinidi.com>
2021-11-25 12:40:33 +0200

    FTL-1999. Fixed integration tests include.

--- sdk/core/test/integration/index.test.ts
+++ sdk/core/test/integration/index.test.ts
@@ -2,8 +2,8 @@ import 'mocha'
 import './env'
 
 require('./migration')
-// require('./services')
-// require('./AffinityWallet.test')
-// require('./NetworkMember.test')
-// require('./otp/index.test')
-// require('./otp-legacy/index.test')
+require('./services')
+require('./AffinityWallet.test')
+require('./NetworkMember.test')
+require('./otp/index.test')
+require('./otp-legacy/index.test')
