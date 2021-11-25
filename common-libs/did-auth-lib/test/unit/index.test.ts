efa23218b4bae90798231bfa0f2a9d5157dba91b
inga-affinidi <82827564+inga-affinidi@users.noreply.github.com>
2021-11-25 18:59:37 +0100

    Fixed did-auth unit test

--- common-libs/did-auth-lib/test/unit/index.test.ts
+++ common-libs/did-auth-lib/test/unit/index.test.ts
@@ -1,6 +1,9 @@
 import { config } from 'dotenv'
 config()
 
+import { useNodeFetch } from '@affinidi/platform-fetch-node'
+useNodeFetch()
+
 require('./DidAuthService/DidAuthRequestToken.test')
 require('./DidAuthService/DidAuthResponseToken.test')
 require('./DidAuthService/DidAuthService.test')
