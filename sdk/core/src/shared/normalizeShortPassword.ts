b2f65139a6b35afeae425fa7ad13cd058345948d
Vitaly R <vitaly.r@affinidi.com>
2021-12-02 12:52:02 +0200

    Added "geenratePassword" to SDK, added return types

--- sdk/core/src/shared/normalizeShortPassword.ts
+++ sdk/core/src/shared/normalizeShortPassword.ts
@@ -2,7 +2,7 @@ import { KeysService } from '@affinidi/common'
 
 import { MINIMUM_RECOMMENDED_PASSWORD_LENGTH } from '../_defaultConfig'
 
-export const normalizeShortPassword = (password: string, login: string) => {
+export const normalizeShortPassword = (password: string, login: string): string => {
   if (password.length < MINIMUM_RECOMMENDED_PASSWORD_LENGTH) {
     const userNameBuffer = KeysService.sha256(Buffer.from(login))
     const userNameHash = userNameBuffer.toString('hex')
