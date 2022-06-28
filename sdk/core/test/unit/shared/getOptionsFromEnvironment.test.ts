c471424b845242d158858145fe1b0256a2e75dc6
Volodymyr Pavlyshyn <volodymyr.p@affinidi.org>
2022-06-28 13:56:50 +0200

    feat:(FTL-5558) fix tests

--- sdk/core/test/unit/shared/getOptionsFromEnvironment.test.ts
+++ sdk/core/test/unit/shared/getOptionsFromEnvironment.test.ts
@@ -2,11 +2,13 @@ import { expect } from 'chai'
 import { getOptionsFromEnvironment } from '../../../src/shared/getOptionsFromEnvironment'
 
 describe('getOptionsFromEnvironment', () => {
+  const apiKey = '12f7b7d9-85d3-43a6-b8ea-4b80bb3fc689'
   it('should accept custom cognito settings', () => {
     const customUserPool = 'userpool_id'
     const customClientId = 'clientid'
     const resultOptions = getOptionsFromEnvironment({
       env: 'prod',
+      apiKey,
       userPoolId: customUserPool,
       clientId: customClientId,
     })
@@ -17,6 +19,7 @@ describe('getOptionsFromEnvironment', () => {
   it('should provide default cognito settings', () => {
     const resultOptions = getOptionsFromEnvironment({
       env: 'prod',
+      apiKey
     })
     expect(resultOptions.basicOptions.clientId).to.exist
     expect(resultOptions.basicOptions.userPoolId).to.exist
@@ -26,6 +29,7 @@ describe('getOptionsFromEnvironment', () => {
     const queryBloomVault = false
     const resultOptions = getOptionsFromEnvironment({
       env: 'prod',
+      apiKey,
       queryBloomVault,
     })
     expect(resultOptions.otherOptions.queryBloomVault).to.equal(queryBloomVault)
