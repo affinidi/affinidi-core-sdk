196015c58ab55dd6ea436ec13690aab68c191915
Volodymyr Pavlyshyn <volodymyr.p@affinidi.org>
2022-11-24 17:03:11 +0100

    fix: key length normalize

--- common-libs/common/src/services/EncryptionService.ts
+++ common-libs/common/src/services/EncryptionService.ts
@@ -1,10 +1,11 @@
 import { randomBytes } from '../shared/randomBytes'
 const aes = require('browserify-aes/browser')
 const aesModes = require('browserify-aes/modes')
+const createHash = require('create-hash/browser')
 
 const ENCRYPTION_ALGORITHM = 'aes-256-cbc'
 const IV_LENGTH = 16
-
+const KEY_LENGTH = 32
 const createCipher = (suite: string, key: unknown, iv: unknown, isDecipher = false) => {
   let cipherType = 'createCipheriv'
   if (isDecipher) {
@@ -28,9 +29,29 @@ const createDecipheriv = (suite: string, key: unknown, iv: unknown) => {
   return createCipher(suite, key, iv, true)
 }
 
+const normalizeKey = (key: string): Buffer | undefined => {
+  const keyBuffer = Buffer.from(key)
+
+  if (!keyBuffer.length) {
+    return undefined
+  }
+
+  const passwordHexBuffer = Buffer.from(key, 'hex')
+
+  if (passwordHexBuffer.length === KEY_LENGTH) {
+    return passwordHexBuffer
+  }
+
+  if (keyBuffer.length !== KEY_LENGTH) {
+    return createHash('sha256').update(key).digest()
+  }
+
+  return keyBuffer
+}
+
 export class EncryptionService {
   static async encrypt(seedHexWithMethod: string, key: string) {
-    const keyBuffer = Buffer.from(key, 'hex')
+    const keyBuffer = normalizeKey(key)
     const seedBuffer = Buffer.from(seedHexWithMethod, 'hex')
     const iv = await randomBytes(IV_LENGTH)
 
@@ -40,12 +61,12 @@ export class EncryptionService {
     return Buffer.concat([iv, encryptedSeed]).toString('hex')
   }
 
-  static decrypt(payload: string, encryptionKey: string) {
-    const encryptedSeedBuffer = Buffer.from(payload, 'hex')
-    const iv = encryptedSeedBuffer.slice(0, IV_LENGTH)
-    const encryptedSeedWtihoutVector = encryptedSeedBuffer.slice(IV_LENGTH)
+  static decrypt(data: string, key: string) {
+    const dataBuffer = Buffer.from(data, 'hex')
+    const iv = dataBuffer.slice(0, IV_LENGTH)
+    const encryptedSeedWtihoutVector = dataBuffer.slice(IV_LENGTH)
 
-    const decipher = createDecipheriv(ENCRYPTION_ALGORITHM, encryptionKey, iv)
+    const decipher = createDecipheriv(ENCRYPTION_ALGORITHM, normalizeKey(key), iv)
     const decryptedBuffer = Buffer.concat([decipher.update(encryptedSeedWtihoutVector), decipher.final()])
     return decryptedBuffer.toString('hex')
   }
