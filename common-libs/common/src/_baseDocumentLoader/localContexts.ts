eb09774cd842c594f5805f27cea1417ab563504e
Volodymyr Pavlyshyn <volodymyr.p@affinidi.org>
2023-03-13 12:44:36 +0100

    feat: add submission context to a document loader

--- common-libs/common/src/_baseDocumentLoader/localContexts.ts
+++ common-libs/common/src/_baseDocumentLoader/localContexts.ts
@@ -273,4 +273,19 @@ export const localContexts: Record<string, Record<string, any>> = {
       },
     },
   },
+  'https://identity.foundation/presentation-exchange/submission/v1': {
+    '@context': {
+      '@version': 1.1,
+      PresentationSubmission: {
+        '@id': 'https://identity.foundation/presentation-exchange/#presentation-submission',
+        '@context': {
+          '@version': 1.1,
+          presentation_submission: {
+            '@id': 'https://identity.foundation/presentation-exchange/#presentation-submission',
+            '@type': '@json',
+          },
+        },
+      },
+    },
+  },
 }
