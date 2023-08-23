c34e05ca247445b2b203394060a0a72141dd290e
Mritunjay Kumar <mritunjaykumar@Mac-C02G429HMD6R.local>
2023-08-23 16:37:38 +0530

    fix: linter error

--- common-libs/common/src/Affinity.ts
+++ common-libs/common/src/Affinity.ts
@@ -85,13 +85,14 @@ export class Affinity {
     if (!isSignatureVerified) {
       throw new Error('Signature on token is invalid')
     }
-
-    if(payload.expiresAt && payload.expiresAt < Date.now()) {
+    if(payload.expiresAt) {
+     if (payload.expiresAt < Date.now()) {
       throw new Error('Token expired')
+     }
     } else {
-      if (payload.exp < Date.now()) {
-        throw new Error('Token expired')
-      }
+     if (payload.exp < Date.now()) {
+      throw new Error('Token expired')
+     }
     }
 
     if (initialEncryptedtoken) {
